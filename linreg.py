322bc60d57dd82595c625551519a9a81be16fedc
Marc Goulding <gouldingmarc@gmail.com>
2022-05-12 07:19:48 +0100

    Create linreg.py

---
+++ linreg.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+df = pd.read_csv(
+    "./data/book_sales.csv",
+    index_col='Date',
+    parse_dates=['Date'],
+).drop('Paperback', axis=1)
+
+print(
+df.head()
+)
+
+import numpy as np
+
+df['Time'] = np.arange(len(df.index))
+
+print(
+df.head()
+)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use("seaborn-whitegrid")
+plt.rc(
+    "figure",
+    autolayout=True,
+    figsize=(11, 4),
+    titlesize=18,
+    titleweight='bold',
+)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=16,
+    titlepad=10,
+)
+# %config InlineBackend.figure_format = 'retina'
+
+fig, ax = plt.subplots()
+ax.plot('Time', 'Hardcover', data=df, color='0.75')
+ax = sns.regplot(x='Time', y='Hardcover', data=df, ci=None, scatter_kws=dict(color='0.25'))
+ax.set_title('Time Plot of Hardcover Sales');
+
+
+
+
+
+plt.show()
